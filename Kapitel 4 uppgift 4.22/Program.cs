cb6454b25eee600e305547f4071d24e3675a46a6
oskr05 <oskr05001@utb.vaxjo.se>
2022-10-19 11:30:24 +0200

    Add project files.

---
+++ Kapitel 4 uppgift 4.22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 200; i > 179; i--)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1000; i <= 1400; i +=50)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
